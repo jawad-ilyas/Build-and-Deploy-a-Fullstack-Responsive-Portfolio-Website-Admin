12c0ea99ae5d0035e54d5a31f09f4b7f4ee6b686
jawad-ilyas <mughaljawad12@gmail.com>
2024-09-27 00:50:58 +0500

    resolve the path issue

--- src/store/Store.js
+++ src/store/Store.js
@@ -6,7 +6,7 @@ import SkillReducer from '../features/skill/Skill.slice.js'
 import ContactReducer from '../features/contact/Contact.slice.js'
 import BrandReducer from '../features/brand/Brand.slice.js'
 import WorkExperienceReducer from '../features/workExperience/WorkExperience.slice.js'
-import experienceReducer from '../features/experience/experience.slice.js'
+import experienceReducer from '../features/experience/Experience.slice.js'
 export const store = configureStore({
     reducer: {
         testimonial: testimonialReducer,
